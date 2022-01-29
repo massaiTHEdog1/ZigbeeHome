adbf8fcc54c39e6694290960f2c2ca2d468495e7
massaiTHEdog <massai77@hotmail.fr>
2022-01-30 00:12:59 +0100

    Multiple modules close #1

--- frontend/src/views/Editor.vue
+++ frontend/src/views/Editor.vue
@@ -22,7 +22,75 @@
 				</draggable-parent-group>
 			</b-col>
 			<b-col class="h-100 p-0">
-				<div ref="editor" style="height: 100%;" @drop="onDrop" @dragover="dragover($event)" @dragenter.prevent @wheel="onWheel"></div>
+				<b-container fluid class="h-100">
+					<b-row>
+						<div class="ml-auto mr-auto">
+							<div class="module" :class="{ 'active' : mod.active }" v-for="mod in modules" :key="mod.label" @click="changeModule(mod.label)">
+								{{mod.label}}
+								<span style="margin-left: 12px;" v-show="mod.label !== 'Home'">
+									<b-icon-pencil-fill class="align-baseline" font-scale="0.8" title="Rename" @click="renameModule(mod)"></b-icon-pencil-fill>
+								</span>
+								<span style="margin-left: 6px;" v-show="mod.label !== 'Home'">
+									<b-icon-trash-fill class="align-baseline text-danger" font-scale="0.8" title="Delete" @click="deleteModule(mod)"></b-icon-trash-fill>
+								</span>
+							</div>
+							<div class="module" title="Add new module" @click="addModule">+</div>
+						</div>
+
+						<b-modal centered 
+							id="modal-module-name"
+							title="Module name"
+							@ok="handleOk"
+							header-bg-variant="dark"
+							body-bg-variant="dark"
+							footer-bg-variant="dark"
+							header-text-variant="light"
+						>
+							<form ref="form" @submit.stop.prevent="handleSubmit">
+								<b-form-group
+								label="Name"
+								label-for="name-input"
+								invalid-feedback="Name is invalid."
+								:state="moduleNameState"
+								>
+								<b-form-input
+									id="name-input"
+									v-model="moduleName"
+									:state="moduleNameState"
+								></b-form-input>
+								</b-form-group>
+							</form>
+						</b-modal>
+
+						<b-modal centered 
+							id="modal-module-name-edit"
+							title="Module name"
+							@ok="handleOkEdit"
+							header-bg-variant="dark"
+							body-bg-variant="dark"
+							footer-bg-variant="dark"
+							header-text-variant="light"
+						>
+							<form ref="form" @submit.stop.prevent="handleSubmitEdit">
+								<b-form-group
+								label="Name"
+								label-for="name-input"
+								invalid-feedback="Name is invalid or already exists."
+								:state="moduleNameEditState"
+								>
+								<b-form-input
+									id="name-input"
+									v-model="moduleNameEdit"
+									:state="moduleNameEditState"
+								></b-form-input>
+								</b-form-group>
+							</form>
+						</b-modal>
+					</b-row>
+					<b-row class="h-100">
+						<div ref="editor" style="height: 100%; width: 100%;" @drop="onDrop" @dragover="dragover($event)" @dragenter.prevent @wheel="onWheel"></div>
+					</b-row>
+				</b-container>
 			</b-col>
 		</b-row>
 	</b-container>
@@ -47,6 +115,7 @@ import ManualTriggerEvent from "@/components/nodes/ManualTriggerEvent.vue";
 import SetVariable from "@/components/nodes/SetVariable.vue";
 import Condition from "@/components/nodes/Condition.vue";
 import { Manager } from "@/main";
+import { BForm, BvModalEvent } from "bootstrap-vue";
 
 require('/node_modules/drawflow/dist/drawflow.min.css')
 
@@ -61,6 +130,7 @@ export default class Editor extends Vue {
 	NodeTypeEnum = NodeTypeEnum;
 	@Ref("editor") editorElement! : HTMLElement;
 	editor! : Drawflow;
+	modules : Array<Module> = [{ label : "Home", active : true }];
 
 	async mounted() {
 		this.editor = new Drawflow(this.editorElement, Vue, this);
@@ -83,7 +153,6 @@ export default class Editor extends Vue {
 		this.editor.on("connectionCreated", this.saveEditor);
 		this.editor.on("connectionRemoved", this.saveEditor);
 		this.editor.on("moduleCreated", this.saveEditor);
-		this.editor.on("moduleChanged", this.saveEditor);
 		this.editor.on("moduleRemoved", this.saveEditor);
 
 		Manager.connection!.on("drawflowReceived", (json : string) => {
@@ -91,6 +160,8 @@ export default class Editor extends Vue {
 			if(!this.isEmpty(json))
 			{
 				this.editor.import(JSON.parse(json));
+				this.ReloadModules();
+				this.changeModule("Home");
 			}
 		});
 
@@ -172,7 +243,6 @@ export default class Editor extends Vue {
 			default:
 				throw new Error(`Node '${NodeTypeEnum[nodeType]}' not implemented !`);
 		}
-		
 	}
 
 	public onWheel(event : WheelEvent)
@@ -186,6 +256,170 @@ export default class Editor extends Vue {
 			this.editor.zoom_in();
 		}
 	}
+
+	public ReloadModules()
+	{
+		this.modules = Object.keys(this.editor?.drawflow?.drawflow).map(x => { return { label : x, active : false}});
+	}
+
+	public changeModule(moduleName : string)
+	{
+		for(let element of this.modules)
+		{
+			element.active = element.label === moduleName;
+		}
+
+		this.editor.changeModule(moduleName);
+	}
+
+	//#region Add module
+
+	moduleName = "";
+	moduleNameState : boolean | null = null;
+
+	public addModule()
+	{
+		this.moduleName = ''
+        this.moduleNameState = null
+		this.$bvModal.show('modal-module-name')
+	}
+
+	public handleOk(event : BvModalEvent) {
+        // Prevent modal from closing
+        event.preventDefault()
+        // Trigger submit handler
+        this.handleSubmit()
+	}
+
+	public handleSubmit() {
+		// Exit when the form isn't valid
+		const valid = !this.isEmpty(this.moduleName) && !this.modules.some(x => x.label === this.moduleName)
+		this.moduleNameState = valid
+
+		if (!valid) {
+			return
+		}
+		
+		this.editor.addModule(this.moduleName);
+
+		this.ReloadModules();
+		this.changeModule(this.moduleName);
+		this.saveEditor();
+
+		// Hide the modal manually
+		this.$nextTick(() => {
+			this.$bvModal.hide('modal-module-name')
+		})
+	}
+
+	//#endregion Add module
+
+	//#region Rename module
+
+	moduleInEdition : Module | null = null;
+	moduleNameEdit = "";
+	moduleNameEditState : boolean | null = null;
+
+	public renameModule(module : any)
+	{
+		this.moduleInEdition = module;
+		this.moduleNameEdit = this.moduleInEdition!.label;
+		this.moduleNameEditState = null;
+		this.$bvModal.show('modal-module-name-edit')
+	}
+
+	public handleOkEdit(event : BvModalEvent) {
+        // Prevent modal from closing
+        event.preventDefault();
+        // Trigger submit handler
+        this.handleSubmitEdit();
+	}
+
+	public handleSubmitEdit() {
+
+		if(this.moduleNameEdit === this.moduleInEdition?.label)//If name is unchanged
+		{
+			this.$nextTick(() => {
+				this.$bvModal.hide('modal-module-name-edit')
+			});
+
+			return;
+		}
+
+		// Exit when the form isn't valid
+		const valid = !this.isEmpty(this.moduleNameEdit) && !this.modules.some(x => x.label === this.moduleNameEdit)
+		this.moduleNameEditState = valid
+
+		if (!valid) {
+			return
+		}
+		
+		const old_key = this.moduleInEdition!.label;
+		const new_key = this.moduleNameEdit;
+
+		//delete Object.assign(this.editor.drawflow.drawflow, {[new_key]: this.editor.drawflow.drawflow[old_key] })[old_key];
+
+		this.editor.drawflow.drawflow = this.RenameKeyInObject(this.editor.drawflow.drawflow, old_key, new_key);
+
+		this.ReloadModules();
+		this.changeModule(new_key);
+		this.saveEditor();
+
+		// Hide the modal manually
+		this.$nextTick(() => {
+			this.$bvModal.hide('modal-module-name-edit');
+		})
+	}
+
+	/** Return the object with a key renamed */
+	RenameKeyInObject(object : any, oldKey : string, newKey : string){
+		const keys = Object.keys(object);
+		const newObj = keys.reduce((acc : any, val)=>{
+			if(val === oldKey){
+				acc[newKey] = object[oldKey];
+			}
+			else {
+				acc[val] = object[val];
+			}
+			return acc;
+		}, {});
+
+		return newObj;
+	}
+
+	//#endregion Rename module
+
+	public async deleteModule(module : Module)
+	{
+		const confirmation = await this.$bvModal.msgBoxConfirm(`Please confirm that you want to delete module '${module.label}'.`, {
+			title: 'Please Confirm',
+			size: 'sm',
+			buttonSize: 'sm',
+			okVariant: 'danger',
+			okTitle: 'YES',
+			cancelTitle: 'NO',
+			footerClass: 'p-2',
+			hideHeaderClose: false,
+			centered: true,
+			headerBgVariant: "dark",
+			bodyBgVariant: "dark",
+			footerBgVariant: "dark",
+			headerTextVariant: "light"
+        });
+
+		if(confirmation === true)
+		{
+			this.editor.removeModule(module.label);
+			this.ReloadModules();
+			this.changeModule("Home");
+		}
+	}
+}
+
+class Module
+{
+	label = "";
+	active = false;
 }
 </script>
 
@@ -206,5 +440,41 @@ export default class Editor extends Vue {
 	.drawflow-node hr
 	{
 		background-color: white;
+	}
+
+	.module
+	{
+		background-color: #303030;
+		color: white;
+		display: inline-block;
+		cursor: pointer;
+		padding: 0.5rem 0.5rem 0.5rem 1rem;
+	}
+
+	.module:not(:first-child)
+	{
+		border-left: 1px solid rgb(61, 61, 61);
+	}
+
+	.module:first-child
+	{
+		padding: 0.5rem 1rem;
+	}
+
+	.module:last-child
+	{
+		padding: 0.5rem 1rem;
+		font-weight: bold;
+	}
+
+	.module:hover
+	{
+		background-color: #2e2e2e;
+	}
+
+	.module.active
+	{
+		background-color: #3e3e3e;
+		font-weight: bold;
 	}
 </style>
